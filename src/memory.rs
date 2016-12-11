34455a15c79d86367694560f827021aa5bb2f59f
Paul Betts <paul@paulbetts.org>
2016-12-10 23:19:03 -0800

    This compiles so it means it works right

--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,11 @@
-use futures;
+use futures::future::*;
 use time;
 
+use std::collections::HashMap;
+use std::io::{Error, ErrorKind};
+use std::iter::FromIterator;
 use time::Timespec;
+use traits::{BlobCache, ErrFuture};
 
 struct CacheEntry {
   created_at: Timespec,
@@ -20,41 +24,68 @@ impl CacheEntry {
 }
 
 pub struct InMemoryBlobCache {
+  data: HashMap<String, CacheEntry>,
 }
 
 impl InMemoryBlobCache {
-   fn new() -> InMemoryBlobCache {
-     return InMemoryBlobCache {}
+   pub fn new() -> InMemoryBlobCache {
+     return InMemoryBlobCache { data: HashMap::new() };
    }
 }
 
-impl BlobCache for InMemoryBlobCache {
-  fn get(key: &str) -> ErrFuture<Vec<u8>> {
-
+impl<'a> BlobCache<'a> for InMemoryBlobCache {
+  fn get(&self, key: &str) -> ErrFuture<Vec<u8>> {
+    if let Some(ce) = self.data.get(key) {
+      match ce.expires_at {
+        Some(e) if e < time::now().to_timespec() => 
+          return failed(Error::new(ErrorKind::Other, "Key not found")).boxed(),
+        _ => return done(Ok(ce.value.clone())).boxed(),
+      }
+    } else {
+      return failed(Error::new(ErrorKind::Other, "Key not found")).boxed();
+    }
   }
 
-  fn get_all_keys() -> ErrFuture<Vec<String>> {
-
+  fn get_all_keys(&self) -> ErrFuture<Vec<String>> {
+    let ret = Vec::from_iter(self.data.keys().map(|x| x.to_owned()));
+    return done(Ok(ret)).boxed();
   }
 
-  fn get_created_at(key: &str, absoluteExpiration: Option<Timespec>) -> ErrFuture<Timespec> {
-
+  fn get_created_at(&self, key: &str) -> ErrFuture<Timespec> {
+    if let Some(x) = self.data.get(key) {
+      return done(Ok(x.created_at)).boxed();
+    } else {
+      return failed(Error::new(ErrorKind::Other, "Key not found")).boxed();
+    }
   }
 
-  fn insert(key : &str, value: &[u8]) -> ErrFuture<bool> {
-
+  fn insert(&mut self, key : &str, value: &[u8], expires_at: Option<Timespec>) -> ErrFuture<bool> {
+    self.data.insert(key.to_owned(), CacheEntry::new(value, None, expires_at));
+    return done(Ok(true)).boxed();
   }
 
-  fn invalidate(key: &str) -> ErrFuture<bool> {
-
+  fn invalidate(&mut self, key: &str) -> ErrFuture<bool> {
+    if let Some(_) = self.data.remove(key) {
+      return done(Ok(true)).boxed();
+    } else {
+      return failed(Error::new(ErrorKind::Other, "Key not found")).boxed();
+    }
   }
 
-  fn invalidate_all() -> ErrFuture<bool> {
-
+  fn invalidate_all(&mut self) -> ErrFuture<bool> {
+    self.data.clear();
+    return done(Ok(true)).boxed();
   }
 
-  fn flush() -> ErrFuture<bool>;
-  fn vacuum() -> ErrFuture<bool>;
-  fn shutdown() -> ErrFuture<bool>;
+  fn flush(&mut self) -> ErrFuture<bool> {
+    return done(Ok(true)).boxed();
+  }
+
+  fn vacuum(&mut self) -> ErrFuture<bool> {
+    return done(Ok(true)).boxed();
+  }
+
+  fn shutdown(&mut self) -> ErrFuture<bool> {
+    return done(Ok(true)).boxed();
+  }
 }
-*/
--- src/traits.rs
+++ src/traits.rs
@@ -3,17 +3,17 @@ use futures;
 
 use time::Timespec;
 
-type ErrFuture<T> = futures::Future<Item = T, Error = std::io::Error>;
+pub type ErrFuture<T> = Box<futures::Future<Item = T, Error = std::io::Error> + Send>;
 
-pub trait BlobCache {
-  fn get(key: &str) -> ErrFuture<Vec<u8>>;
-  fn get_all_keys() -> ErrFuture<Vec<String>>;
-  fn get_created_at(key: &str, absoluteExpiration: Option<Timespec>) -> ErrFuture<Timespec>;
-  fn insert(key : &str, value: &[u8]) -> ErrFuture<bool>;
-  fn invalidate(key: &str) -> ErrFuture<bool>;
-  fn invalidate_all() -> ErrFuture<bool>;
+pub trait BlobCache<'a> {
+  fn get(&self, key: &str) -> ErrFuture<Vec<u8>>;
+  fn get_all_keys(&self) -> ErrFuture<Vec<String>>;
+  fn get_created_at(&self, key: &str) -> ErrFuture<Timespec>;
+  fn insert(&mut self, key : &str, value: &[u8], expires_at: Option<Timespec>) -> ErrFuture<bool>;
+  fn invalidate(&mut self, key: &str) -> ErrFuture<bool>;
+  fn invalidate_all(&mut self) -> ErrFuture<bool>;
 
-  fn flush() -> ErrFuture<bool>;
-  fn vacuum() -> ErrFuture<bool>;
-  fn shutdown() -> ErrFuture<bool>;
+  fn flush(&mut self) -> ErrFuture<bool>;
+  fn vacuum(&mut self) -> ErrFuture<bool>;
+  fn shutdown(&mut self) -> ErrFuture<bool>;
 }
