22197b9aade16d9e1b9f9111cf50af11e9f3307d
Paul Betts <paul@paulbetts.org>
2016-12-19 15:04:47 -0800

    Akavache caches are thread-safe

--- src/memory.rs
+++ src/memory.rs
@@ -4,6 +4,7 @@ use time;
 use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 use std::iter::FromIterator;
+use std::sync::Mutex;
 use time::Timespec;
 use traits::{BlobCache, ErrFuture};
 
@@ -25,16 +26,19 @@ impl CacheEntry {
 
 pub struct InMemoryBlobCache {
   data: HashMap<String, CacheEntry>,
+  lock: Mutex<bool>,
 }
 
 impl InMemoryBlobCache {
    pub fn new() -> InMemoryBlobCache {
-     return InMemoryBlobCache { data: HashMap::new() };
+     return InMemoryBlobCache { data: HashMap::new(), lock: Mutex::new(false) };
    }
 }
 
 impl<'a> BlobCache<'a> for InMemoryBlobCache {
-  fn get(&self, key: &str) -> ErrFuture<Vec<u8>> {
+  fn get(&mut self, key: &str) -> ErrFuture<Vec<u8>> {
+    let _l = self.lock.lock().unwrap();
+
     if let Some(ce) = self.data.get(key) {
       match ce.expires_at {
         Some(e) if e < time::now().to_timespec() => 
@@ -46,12 +50,15 @@ impl<'a> BlobCache<'a> for InMemoryBlobCache {
     }
   }
 
-  fn get_all_keys(&self) -> ErrFuture<Vec<String>> {
+  fn get_all_keys(&mut self) -> ErrFuture<Vec<String>> {
+    let _l = self.lock.lock().unwrap();
     let ret = Vec::from_iter(self.data.keys().map(|x| x.to_owned()));
     return done(Ok(ret)).boxed();
   }
 
-  fn get_created_at(&self, key: &str) -> ErrFuture<Timespec> {
+  fn get_created_at(&mut self, key: &str) -> ErrFuture<Timespec> {
+    let _l = self.lock.lock().unwrap();
+
     if let Some(x) = self.data.get(key) {
       return done(Ok(x.created_at)).boxed();
     } else {
@@ -60,11 +67,15 @@ impl<'a> BlobCache<'a> for InMemoryBlobCache {
   }
 
   fn insert(&mut self, key : &str, value: &[u8], expires_at: Option<Timespec>) -> ErrFuture<bool> {
+    let _l = self.lock.lock().unwrap();
+
     self.data.insert(key.to_owned(), CacheEntry::new(value, None, expires_at));
     return done(Ok(true)).boxed();
   }
 
   fn invalidate(&mut self, key: &str) -> ErrFuture<bool> {
+    let _l = self.lock.lock().unwrap();
+
     if let Some(_) = self.data.remove(key) {
       return done(Ok(true)).boxed();
     } else {
@@ -73,19 +84,24 @@ impl<'a> BlobCache<'a> for InMemoryBlobCache {
   }
 
   fn invalidate_all(&mut self) -> ErrFuture<bool> {
+    let _l = self.lock.lock().unwrap();
+
     self.data.clear();
     return done(Ok(true)).boxed();
   }
 
   fn flush(&mut self) -> ErrFuture<bool> {
+    let _l = self.lock.lock().unwrap();
     return done(Ok(true)).boxed();
   }
 
   fn vacuum(&mut self) -> ErrFuture<bool> {
+    let _l = self.lock.lock().unwrap();
     return done(Ok(true)).boxed();
   }
 
   fn shutdown(&mut self) -> ErrFuture<bool> {
+    let _l = self.lock.lock().unwrap();
     return done(Ok(true)).boxed();
   }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -5,10 +5,10 @@ use time::Timespec;
 
 pub type ErrFuture<T> = Box<futures::Future<Item = T, Error = std::io::Error> + Send>;
 
-pub trait BlobCache<'a> {
-  fn get(&self, key: &str) -> ErrFuture<Vec<u8>>;
-  fn get_all_keys(&self) -> ErrFuture<Vec<String>>;
-  fn get_created_at(&self, key: &str) -> ErrFuture<Timespec>;
+pub trait BlobCache<'a> : Send+Sync {
+  fn get(&mut self, key: &str) -> ErrFuture<Vec<u8>>;
+  fn get_all_keys(&mut self) -> ErrFuture<Vec<String>>;
+  fn get_created_at(&mut self, key: &str) -> ErrFuture<Timespec>;
   fn insert(&mut self, key : &str, value: &[u8], expires_at: Option<Timespec>) -> ErrFuture<bool>;
   fn invalidate(&mut self, key: &str) -> ErrFuture<bool>;
   fn invalidate_all(&mut self) -> ErrFuture<bool>;
